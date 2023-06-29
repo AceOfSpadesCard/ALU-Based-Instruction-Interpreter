a0655e4640b12aa3a226ff749778d83b8d4d3b07
AceOfSpades <133607593+AceOfSpadesCard@users.noreply.github.com>
2023-06-29 14:26:46 -0700

    Update Program.py
    Got the ClockCycle Function working, and was ale to integrate it with the while loop that is running, and set up the Internal LED at Pin 25 to Blink at each clock cycle.

--- Program.py
+++ Program.py
@@ -1,1 +1,35 @@
+from machine import Pin
+from time import sleep
 
+# Initializing the primary variables
+Reset = False
+
+
+# Defining Pin 25 (Linked to the Internal LED) on the Raspberry Pi Pico for us to send a signal out
+LEDPin25 = Pin(25, Pin.OUT)
+
+# Defining the Clock Function which determines the speed of the CPU
+def ClockCycle(Hertz):
+    # 1 Hertz = 1 Clock Cycle in 1 Second
+    # Therfore a clock speed of 5 Hertz means 5 Clock Cycles completed in 1 Second
+    
+    
+    # The Speed variable is the number we will enter in the time.sleep function
+    Speed = 1 / float(Hertz)
+    
+    sleep(Speed)
+    
+    # Toggling the LED Pin 25 to turn on every cycle
+    LEDPin25.toggle()
+        
+        
+# The CPU Running in a loop until the reset command has been entered
+
+Count = 0
+
+while Reset != True:
+    # User defined clock cycle speed
+    ClockCycle(1)
+    
+    print(Count)
+    Count += 1
