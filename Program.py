84da0f7bdc0dcb6a46baa3d57cfc92d58c7079a7
AceOfSpades <133607593+AceOfSpadesCard@users.noreply.github.com>
2023-06-30 16:26:30 -0700

    Update Program.py
    - Changed the Program Memory to a List.
    - Created the ArthmeticLogicUnit Class with Addition Functionality
    - Infinitely many parameters accepted
    - Create a Subtraction function in the ArthmeticLogicUnit Class
    - Create the ALU Interpreter Interface
    - What able to create the ADD Command on the Shell.

--- Program.py
+++ Program.py
@@ -4,41 +4,144 @@ from time import sleep
 # Initializing the primary variables
 Exit = False
 UserInputCheck = True
+ALUPoint = False
 
-# The Count of how many clock cycles have occured in the program
-CycleCount = 0
+# ROM Memory #
+Documentation = ()
+##############
+
+# RAM Memory #
+ProgramMemory = []
+##############
+
+# Creating the Arithmetic Logic Unit Class
+class ArithmeticLogicUnit:
+    def __init__(self):
+        pass
+    
+    # The addition function that performs addition on as many numbers as you want
+    def Addition(self, Numbers):
+        Total = 0.0
+        for Number in Numbers:
+            Total += Number
+        print(Total)
+        
+    # The subtraction function that performs subraction on as many numbers as you want
+    def Subtraction(self, Numbers):
+        Primary = Numbers[0]
+        Numbers.pop(0)
+        for Number in Numbers:
+            Primary -= Number
+        print(Primary)
 
 # Defining Pin 25 (Linked to the Internal LED) on the Raspberry Pi Pico for us to send a signal out
 LEDPin25 = Pin(25, Pin.OUT)
 
+# Count of times the clock cycle has occured
+CycleCount = 0
+
+# The CPU Running in a loop until the exit command has been entered
+
+ALU = ArithmeticLogicUnit()
+
+# User defines the computers clock speed
+ClockSpeed = float(input("DEFINE CLOCK SPEED [HERTZ]: "))
+
 # Defining the Clock Function which determines the speed of the CPU
-def ClockCycle(Hertz):
+def ClockCycle():
     # 1 Hertz = 1 Clock Cycle in 1 Second
     # Therfore a clock speed of 5 Hertz means 5 Clock Cycles completed in 1 Second
     
     
     # The Speed variable is the number we will enter in the sleep function
-    Speed = 1 / float(Hertz)
+    Speed = 1 / float(ClockSpeed)
     
-    sleep(Speed)
-    
-    # Toggling the LED Pin 25 to turn on every cycle
     LEDPin25.toggle()
-        
-        
-# The CPU Running in a loop until the exit command has been entered
+    
+    sleep(Speed/2)
+    
+    LEDPin25.toggle()
 
+    sleep(Speed/2)
+    
+# Defining Check Minor Variables
+ALUInputPoint = False
+ALUExitPoint = False
 
 while Exit != True:
-    # User defined clock cycle speed
-    ClockCycle(1)
+    ClockCycle()
     CycleCount += 1
     
+    if ALUPoint == True:
+        if ALUInputPoint == True:
+            # ALU Command Shell
+            ALUInput = input("|ALU| --> ")
+            
+            # If the EXIT Command is entered then break from the loop
+            if "EXIT" in ALUInput:
+                ALUPoint = False
+                UserInputCheck = True
+                
+                # Clock Cycle Pulse
+                ClockCycle()
+                CycleCount += 1
+                
+            # If the ADD Command is present, then commence the ADD Procedure
+            elif ALUInput[0:3] == "ADD":
+                
+                # Clock Cycle Pulse
+                ClockCycle()
+                CycleCount += 1
+                
+                # Retrieve all the numbers after the ADD Command
+                AdditionNumbers = ALUInput[4:]
+                    
+                # Clock Cycle Pulse
+                ClockCycle()
+                CycleCount += 1
+                
+                # Split them into a list, based on the comma
+                RawAdditionNumbers = AdditionNumbers.split(",")
+                FinalAdditionNumbers = []
+                
+                # Clock Cycle Pulse
+                ClockCycle()
+                CycleCount += 1
+                    
+                # Run a loop iterating through the list, and changing their data type to a float
+                for AdditionNumber in RawAdditionNumbers:
+                    AdditionNumber = float(AdditionNumber)
+
+                    # Then appending them into the new list, FinalAdditionNumbers which will be passed as a argument to the ALU Class
+                    FinalAdditionNumbers.append(float(AdditionNumber))
+                        
+                # Clock Cycle Pulse
+                ClockCycle()
+                CycleCount += 1
+                
+                # Call the Addition Function from the ALU Class
+                ALU.Addition(FinalAdditionNumbers)
+                ALUInputPoint = True
+        
+                
     # If the UserInputCheck Variable
-    if UserInputCheck == True:
+    elif UserInputCheck == True:
         UserInput = input("### ")
         
+        # Clock Cycle Pulse
+        ClockCycle()
+        CycleCount += 1
+        
         # If the 'EXIT' command is entered then set the Exit variable to True
-        if UserInput == "EXIT" : Exit = True
+        if UserInput == "EXIT" :
+            Exit = True
+
+        elif UserInput == "|ALU|":
+            ALUPoint = True
+            ALUInputPoint = True
+            UserInputCheck = False
+        
+        else:
+            print(UserInput)
         
         
