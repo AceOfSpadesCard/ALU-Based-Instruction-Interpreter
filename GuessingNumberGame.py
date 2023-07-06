3b7088171549ddd8bb3673aa8c806dedc8fd916a
AceOfSpades <133607593+AceOfSpadesCard@users.noreply.github.com>
2023-07-05 21:08:03 -0700

    Create GuessingNumberGame.py

---
+++ GuessingNumberGame.py
@@ -0,0 +1,17 @@
+InstructionMemory = (
+    "WRITE RegisterA Welcome to the number guessing game!",
+    "WRITE RegisterB You have only three tries to guess the secret number.",
+    "WRITE RegisterC The number is a integer between 1 and 20.",
+    "WRITE RegisterD Good luck and enjoy!",
+    "WRITE RegisterE 14",
+    "READ RegisterA",
+    "READ RegisterB",
+    "READ RegisterC",
+    "READ RegisterD",
+    "CollectINT Guess: ",
+    "CompareR=E,I",
+    "CollectINT Guess: ",
+    "CompareR=E,I",
+    "CollectINT Guess: ",
+    "CompareR=E,I",
+    )
